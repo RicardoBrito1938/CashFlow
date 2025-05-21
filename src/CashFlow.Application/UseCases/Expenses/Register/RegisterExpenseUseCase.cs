63d1062b52a4ada6fc26687c5d1d08139a492a14
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-05-21 16:06:42 +0100

    feat: validation

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,3 +1,4 @@
+using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
@@ -7,7 +8,41 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestExpenseJson request)
     {
-        //TODO Validation 
+        Validate(request);
         return new ResponseRegisteredExpenseJson();
+    }
+    
+    private void Validate(RequestExpenseJson request)
+    {
+        var titleEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleEmpty)
+            throw new ArgumentException(
+                "The title is required",
+                nameof(request.Title)
+            );
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException(
+                "The amount must be greater than zero",
+                nameof(request.Amount)
+            );
+        }
+
+        var dateResult = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (dateResult > 0)
+        {
+            throw new ArgumentException(
+                "The date must be less than or equal to today",
+                nameof(request.Date)
+            );
+        }
+        var isValidPaymentType = Enum.IsDefined(typeof(PaymentsType), request.PaymentType);
+        if (!isValidPaymentType)
+        {
+            throw new ArgumentException(
+                "The payment type is invalid",
+                nameof(request.PaymentType)
+            );
+        }
     }
 }
