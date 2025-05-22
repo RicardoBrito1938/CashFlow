9c3462de785ef1315c5214c2305a0d9cdcc620f5
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-05-22 22:13:52 +0100

    feat: filter errors pattern

---
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,39 @@
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.Api.Filters;
+
+public class ExceptionFilter: IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException e)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            ThrowUnknownException(context);
+        }
+    }
+    
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            var e = (ErrorOnValidationException)context.Exception;
+            var errorResponse = new ResponseErrorJson(e.Errors);
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+    }
+    
+    private void ThrowUnknownException(ExceptionContext context)
+    {
+        var errorResponse = new ResponseErrorJson("An unexpected error occurred.");
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+}
