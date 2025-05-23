8f135ac28272893afdc9f1b4e71fe88032bce42a
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-05-23 21:01:44 +0100

    feature: culture middleware

---
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,28 @@
+using System.Globalization;
+
+namespace CashFlow.Api.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+    
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next ?? throw new ArgumentNullException(nameof(next));
+    }
+    
+    public async Task Invoke(HttpContext context)
+    {
+        var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+        var cultureInfo = new CultureInfo("en");
+        
+        if (!string.IsNullOrEmpty(culture))
+        { 
+            cultureInfo = new CultureInfo(culture);
+        }
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+        
+        await _next(context);
+    }
+}
