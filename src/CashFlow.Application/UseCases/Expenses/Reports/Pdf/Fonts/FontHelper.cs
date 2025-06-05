15d8f16e710711cc164f10306d8e2b9848bb41e4
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-06-05 13:57:48 +0100

    feat: refactor font handling for PDF reports and implement font resolution

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/FontHelper.cs
@@ -1,4 +1,4 @@
-namespace CashFlow.Application.UseCases.Expenses.Reports.Fonts;
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 
 public class FontHelper
 {
@@ -6,5 +6,7 @@ public class FontHelper
     public const string RALEWAY_BLACK = "RaleWay-Black";
     public const string WORKSANS_REGULAR = "WorkSans-Regular";
     public const string WORKSANS_BLACK = "WorkSans-Black";
+    
+    public const string DEFAULT_FONT = RALEWAY_REGULAR;
    
 }
