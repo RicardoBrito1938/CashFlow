889bafd1ca05fb65b6ef568999ba9a4c0bf4a906
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-06-11 13:59:43 +0100

    chore: add Microsoft.EntityFrameworkCore.Design package and update dependencies to latest versions

--- src/CashFlow.Api/obj/Debug/net9.0/CashFlow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net9.0/CashFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c0fae11a4bd30ca20ac1b0ca251c3947d23e2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7d73fb61ee3f2dd3e4ee46593ca2cbbb45f84e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/Validators.Tests/obj/Debug/net9.0/Validators.Tests.AssemblyInfo.cs
+++ tests/Validators.Tests/obj/Debug/net9.0/Validators.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c0fae11a4bd30ca20ac1b0ca251c3947d23e2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7d73fb61ee3f2dd3e4ee46593ca2cbbb45f84e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Validators.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
