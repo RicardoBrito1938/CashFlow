866ccfbba3a56979851dfed27e4fe6c7ad187803
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-06-12 12:11:56 +0100

    feat: implement password validation logic with regex checks for complexity requirements

--- src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
+++ src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
@@ -1,14 +1,38 @@
+using System.Text.RegularExpressions;
+using CashFlow.Exception;
 using FluentValidation;
 using FluentValidation.Validators;
 
 namespace CashFlow.Application.UseCases.Users;
 
-public class PasswordValidator<T>: PropertyValidator<T, string>
+public partial class PasswordValidator<T>: PropertyValidator<T, string>
 {
+    private const string ERROR_MESSAGE = "ErrorMessage";
+
+    protected override string GetDefaultMessageTemplate(string errorCode)
+    {
+        return $"{{{ERROR_MESSAGE}}}";
+    }
     public override bool IsValid(ValidationContext<T> context, string password)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(password) || password.Length < 8 || UppercaseLetters().IsMatch(password) == false || LowerCaseLetters().IsMatch(password) == false || NumbersRegex().IsMatch(password) == false || MyRegex().IsMatch(password) == false)
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE, ResourceErrorMessages.PASSWORD_INVALID);
+            return false;
+        }
+
+
+        return true;
     }
 
     public override string Name { get; } = "PasswordValidator";
+
+    [GeneratedRegex(@"[A-Z]+")]
+    private static partial Regex UppercaseLetters();
+    [GeneratedRegex(@"[a-z]+")]
+    private static partial Regex LowerCaseLetters();
+    [GeneratedRegex(@"[0-9]+")]
+    private static partial Regex NumbersRegex();
+    [GeneratedRegex(@"[\W_]+")]
+    private static partial Regex MyRegex();
 }
