ed7b97d870b17713d1365872c2656ec2caf7218d
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-06-23 22:33:13 +0100

    test: add error handling test for invalid login in `DoLoginTest`

--- tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
+++ tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
@@ -1,8 +1,13 @@
+using System.Globalization;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 using CashFlow.Communication.Requests;
+using CashFlow.Exception;
+using CommonTestUtils.Requests;
 using Shouldly;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.Login.DoLogin;
 
@@ -29,5 +34,21 @@ public class DoLoginTest(CustomWebApplicationFactory factory): IClassFixture<Cus
         var responseData = await JsonDocument.ParseAsync(responseBody);
         responseData.RootElement.GetProperty("token").GetString().ShouldNotBeNullOrEmpty();
         responseData.RootElement.GetProperty("name").GetString().ShouldBe(_name);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task ErrorLoginInvalid(string language)
+    {
+       var request = RequestLoginJsonBuilder.Build();
+       _client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(language));
+       var response = await _client.PostAsJsonAsync(Method, request);
+         response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+         
+         var responseBody = await response.Content.ReadAsStreamAsync();
+         var responseData = await JsonDocument.ParseAsync(responseBody);
+         var errors = responseData.RootElement.GetProperty("errorMessages").EnumerateArray();
+         var expectedMessageError = ResourceErrorMessages.ResourceManager.GetString("PASSWORD_OR_EMAIL_INVALID", new CultureInfo(language));
+         errors.First().GetString().ShouldBe(expectedMessageError);
     }
 }
