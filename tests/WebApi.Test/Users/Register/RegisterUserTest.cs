c09e6ec588542610b8f74ef6b81683fc522049c5
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-06-23 14:33:06 +0100

    test: add validation test for missing `name` in `RegisterUserTest`
    - Introduced `Error_Name_Required` test case to ensure API returns `400 BadRequest` when the `name` field is missing in the registration request.
    - Verified the presence of appropriate error messages in the response.

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using CashFlow.Exception;
 using CommonTestUtils.Requests;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Shouldly;
@@ -23,4 +24,19 @@ public class RegisterUserTest(CustomWebApplicationFactory factory) : IClassFixtu
         response.RootElement.GetProperty("name").GetString().ShouldBe(request.Name);
         response.RootElement.GetProperty("token").GetString().ShouldNotBeNullOrEmpty();
     }
+
+    [Fact]
+    public async Task Error_Name_Required()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+        var result = await _client.PostAsJsonAsync(Method, request);
+        result.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+        var body = await result.Content.ReadAsStreamAsync();
+        var response = await JsonDocument.ParseAsync(body);
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+        errors.Count().ShouldBe(1);
+        errors.First().GetString().ShouldBe(ResourceErrorMessages.NAME_REQUIRED);
+    }
+    
 }
