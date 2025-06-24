008810335f4e9b770ab1c12444f5a376e92bf52e
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-06-24 09:44:55 +0100

    feat: add `ILoggedUser` interface and `LoggedUser` implementation
    - Introduced `ILoggedUser` interface in the domain layer to define a contract for retrieving the logged-in user.
    - Implemented `LoggedUser` in the infrastructure layer, utilizing JWT tokens to resolve the current user's details from the database.

---
+++ src/CashFlow.Infra/Services/LoggedUser/LoggedUser.cs
@@ -0,0 +1,20 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using CashFlow.Domain.Entities;
+using CashFlow.Domain.Services.LoggedUser;
+using CashFlow.Infra.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infra.Services.LoggedUser;
+
+public class LoggedUser(CashFlowDbContext dbContext): ILoggedUser
+{
+    public async Task<User> Get()
+    {
+        string token = "token";
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+        var identifier = jwtSecurityToken.Claims.First(claim => claim.Type == ClaimTypes.Sid).Value;
+        return await dbContext.Users.AsNoTracking().FirstAsync(user => user.UserIdentifier == Guid.Parse(identifier));
+    }
+}
