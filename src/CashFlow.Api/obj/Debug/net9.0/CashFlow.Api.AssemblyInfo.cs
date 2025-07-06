1229027d7483124e7b77a6a1948ae2ceb3546492
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-07-06 23:54:43 +0100

    test: add integration tests for `GenerateExpenseReports`
    - Introduced `GenerateExpenseReportsTest` to validate PDF and Excel report generation via `/api/Report`.
    - Added `GetDate` method to `ExpenseIdentityManager` for consistent date usage in tests.
    - Updated configuration and source files to reflect new tests.

--- src/CashFlow.Api/obj/Debug/net9.0/CashFlow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net9.0/CashFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45494593bcfedafd001c46bbf80f3e6ecf3b17d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fbbda175c2c47abb50ec89415e189684a5a155")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ tests/WebApi.Test/Expenses/Reports/GenerateExpenseReportsTest.cs
@@ -0,0 +1,39 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Shouldly;
+
+namespace WebApi.Test.Expenses.Reports.Excel;
+
+public class GenerateExpenseReportsTest: CashFlowClassFixture
+{
+    private const string Method = "api/Report";
+
+    private readonly string _adminToken;
+    private readonly string _userToken;
+    private readonly DateTime _expenseDate;
+    
+    public GenerateExpenseReportsTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _adminToken = factory.User_Admin.GetToken();
+        _userToken = factory.User_Team_Member.GetToken();
+        _expenseDate = factory.Expense_Admin.GetDate();
+    }
+    
+    [Fact]
+    private async Task Success_Pdf()
+    {
+        var result = await DoGet(requestUri: $"{Method}/pdf?month={_expenseDate:Y}", token: _adminToken);
+        result.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
+        var body = await result.Content.ReadAsStreamAsync();
+        body.ShouldNotBeNull();
+    }
+    
+    [Fact]
+    private async Task Success_Excel()
+    {
+        var result = await DoGet(requestUri: $"{Method}/excel?month={_expenseDate:Y}", token: _adminToken);
+        result.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
+        var body = await result.Content.ReadAsStreamAsync();
+        body.ShouldNotBeNull();
+    }
+}
