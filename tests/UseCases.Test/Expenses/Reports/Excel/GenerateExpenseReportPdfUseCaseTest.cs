03f73769526d2a1fac14864d8e3ca7f912146046
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-07-06 23:43:39 +0100

    test: add unit tests for `GenerateExpenseReportPdfUseCase` and `GenerateExpenseExcelPdfUseCase`
    - Introduced `GenerateExpenseReportPdfUseCaseTest` and `GenerateExpenseExcelPdfUseCaseTest` to validate PDF and Excel report generation logic.
    - Enhanced `ExpenseReadOnlyRepositoryBuilder` with month filtering for report generation.
    - Updated test configuration to include new test cases.

---
+++ tests/UseCases.Test/Expenses/Reports/Excel/GenerateExpenseReportPdfUseCaseTest.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+using CashFlow.Domain.Entities;
+using CommonTestUtils.Entities;
+using CommonTestUtils.LoggedUser;
+using CommonTestUtils.Repositories;
+using Shouldly;
+
+namespace UseCases.Test.Expenses.Reports.Excel;
+
+public class GenerateExpenseReportPdfUseCaseTest
+{
+    [Fact]
+    private async Task Success()
+    {
+        // Arrange
+        var user = UserBuilder.Build();
+        var expenses = ExpenseBuilder.Collection( user);        var useCase = CreateUseCase(user, expenses);
+
+        // Act
+        var result = await useCase.Execute(DateOnly.FromDateTime(DateTime.Today));
+
+        // Assert
+        result.ShouldNotBeNull();
+    }
+    
+    private GenerateExpensesReportPdfUseCase CreateUseCase(User user, List<Expense> expenses)
+    {
+        var repository = new ExpenseReadOnlyRepositoryBuilder().FilterByMonth(user, expenses).Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        return new GenerateExpensesReportPdfUseCase(repository, loggedUser);
+    }
+}
---
+++ tests/UseCases.Test/Expenses/Reports/Pdf/GenerateExpenseExcelPdfUseCaseTest.cs
@@ -0,0 +1,33 @@
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Domain.Entities;
+using CommonTestUtils.Entities;
+using CommonTestUtils.LoggedUser;
+using CommonTestUtils.Repositories;
+using Shouldly;
+
+namespace UseCases.Test.Expenses.Reports.Pdf;
+
+public class GenerateExpenseExcelPdfUseCaseTest
+{
+    [Fact]
+    private void Success()
+    {
+        // Arrange
+        var user = UserBuilder.Build();
+        var expenses = ExpenseBuilder.Collection( user);
+        var useCase = CreateUseCase(user, expenses);
+
+        // Act
+        var result = useCase.Execute(DateOnly.FromDateTime(DateTime.Today));
+
+        // Assert
+        result.ShouldNotBeNull();
+    }
+    
+    private GeneratedExpensesReportExcelUseCase CreateUseCase(User user, List<Expense> expenses)
+    {
+        var repository = new ExpenseReadOnlyRepositoryBuilder().FilterByMonth(user, expenses).Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        return new GeneratedExpensesReportExcelUseCase(repository, loggedUser);
+    }
+}
