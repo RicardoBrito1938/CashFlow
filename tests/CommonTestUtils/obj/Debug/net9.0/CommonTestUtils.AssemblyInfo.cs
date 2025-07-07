99b68edaba39326817e5ca95f932cf63f5099cf2
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-07-07 08:44:05 +0100

    test: add unit tests for `GetUserProfileUseCase`
    - Introduced `GetUserProfileUseCaseTest` to validate user profile retrieval.
    - Added success scenario to ensure correct profile data is returned.
    - Updated test configuration and source files.

--- tests/CommonTestUtils/obj/Debug/net9.0/CommonTestUtils.AssemblyInfo.cs
+++ tests/CommonTestUtils/obj/Debug/net9.0/CommonTestUtils.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonTestUtils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f162a62ae5ea3798923304000387c898bb8580b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+892b9f96774bc4969b57e6bec0bf8eca5b60bc19")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonTestUtils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonTestUtils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ tests/UseCases.Test/Users/Profile/GetUserProfileUseCaseTest.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Application.UseCases.Users.Porfile;
+using CashFlow.Domain.Entities;
+using CommonTestUtils.Entities;
+using CommonTestUtils.LoggedUser;
+using CommonTestUtils.Mapper;
+using Shouldly;
+
+namespace UseCases.Test.Users.Profile;
+
+public class GetUserProfileUseCaseTest
+{
+   
+   [Fact]
+   public async Task Success_Get_User_Profile()
+   {
+      var user = UserBuilder.Build();
+      var useCase = CreateUseCase(user);
+      var response = await useCase.Execute();
+      response.ShouldNotBeNull();
+      response.Name.ShouldBe(user.Name);
+      response.Email.ShouldBe(user.Email);
+   }
+
+
+   private GetUserProfileUseCase CreateUseCase(User user)
+   {
+      var mapper = MapperBuilder.Build();
+      var loggedUser = LoggedUserBuilder.Build(user);
+      return new GetUserProfileUseCase(loggedUser, mapper);
+   }
+}
