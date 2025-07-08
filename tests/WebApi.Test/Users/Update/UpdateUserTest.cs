583c8249c669a5dab76788ba9d47f28f3fdb7326
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-07-08 21:58:15 +0100

    test: add integration test for `UpdateUserProfile` API
    - Introduced `UpdateUserTest` to validate `UpdateUserProfile` API behavior.
    - Added success scenario to ensure profile updates return `NoContent`.

---
+++ tests/WebApi.Test/Users/Update/UpdateUserTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Text.Json;
+using CommonTestUtils.Requests;
+using Shouldly;
+
+namespace WebApi.Test.Users.Update;
+
+public class UpdateUserTest:CashFlowClassFixture
+{
+    private const string Method = "api/User";
+    private readonly string _token;
+    
+    public UpdateUserTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _token = factory.User_Team_Member.GetToken();
+    }
+    
+    [Fact]
+    public async Task Success_Update_User_Profile()
+    {
+        var request = RequestUpdateUserJsonBuilder.Build();
+        var response = await DoPut(Method, request, token: _token);
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+}
