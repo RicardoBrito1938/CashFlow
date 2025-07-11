8c636b45216c4927d48d9a051a6b6f044f636c50
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-07-11 23:10:07 +0100

    feat: add Tag enum and entity to support expense categorization
    - Introduced `Tag` enum for predefined expense categories.
    - Created `Tag` entity with relationships to expenses.

---
+++ src/CashFlow.Domain/Enums/Tag.cs
@@ -0,0 +1,15 @@
+﻿namespace CashFlow.Domain.Enums;
+
+public enum Tag
+{
+    Health = 0,
+    Food = 1,
+    Transport = 2,
+    Entertainment = 3,
+    Shopping = 4,
+    Bills = 5,
+    Education = 6,
+    Salary = 7,
+    Investment = 8,
+    Other = 9
+}
